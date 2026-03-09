d71dd35886df1dd8549f038ecafa5249f5a993ac
Mamy-cloud <mamycompteprofessionnel@gmail.com>
2026-03-09 16:50:41 +0300

    maj final 9 mars nom back-end

--- app/postgreSql/synchrone/method_crud/put/put_modify_value_row_postgre_sync.py
+++ app/postgreSql/synchrone/method_crud/put/put_modify_value_row_postgre_sync.py
@@ -2,7 +2,7 @@ from fastapi import APIRouter, HTTPException
 from psycopg2 import sql
 
 from app.postgreSql.synchrone.connexion_db.Postgre_sync_web import postgre_sync_connect_to_db
-from app.postgreSql.synchrone.request.Request_PostgreSql import request_update_row_postgre_sync
+from app.postgreSql.synchrone.request.Request_PostgreSql_Sync_Crud import request_update_row_postgre_sync
 from app.postgreSql.synchrone.json_base_model.model_modify_value_row_postgre_sync import ModifyValueRowModelPostgreSync
 
 router = APIRouter()
